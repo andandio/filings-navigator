d23abe4559af7d5f711ae19b52717341eaebc027
Anders Howerton <anders.e.howerton@gmail.com>
2023-10-30 12:30:27 -0700

    remove unused column from awards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_30_171916) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_192822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,7 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_30_171916) do
     t.bigint "filing_id", null: false
     t.text "purpose"
     t.decimal "cash_award", precision: 10, scale: 2
-    t.text "notes"
     t.index ["filing_id"], name: "index_awards_on_filing_id"
   end
 
